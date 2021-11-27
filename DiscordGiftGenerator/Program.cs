0e1628e358266d4bd2a14ca3e922aafb8217861d
SPFck <73368499+20cmDuro@users.noreply.github.com>
2021-11-27 19:33:41 +0000

    Project Files

---
+++ DiscordGiftGenerator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DiscordGiftGenerator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Utils utils = new Utils();
+            int quantia;
+            StreamWriter sw = File.AppendText("./gifts.txt");
+            Console.WriteLine("<--|   Gerador de Gifts Discord   |-->\n<--|  https://github.com/20cmDuro |-->");
+            Console.Write("Quantia: ");
+            quantia = int.Parse(Console.ReadLine());
+            for(int i = 0; i != quantia; i++) {
+                sw.WriteLine(utils.gen_gift());
+            }
+            sw.Close();
+            Console.WriteLine($"Gifts gerados com sucesso!\nOs Gifts foram salvos em 'gifts.txt'.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DiscordGiftGenerator/Utils.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DiscordGiftGenerator {
+    internal class Utils {
+        private readonly Random rdn = new Random();
+        private string gift_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        public string gen_gift() {
+            return $"https://discord.gift/{new string(Enumerable.Range(1, 16).Select(str => gift_chars[rdn.Next(gift_chars.Length)]).ToArray())}";
+        }
+    }
+}
